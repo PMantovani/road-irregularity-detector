4d4c0c577542fdf432ff628de5ce29a2541e200f
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2017-10-20 20:17:14 -0200

    add KeyboardInterrupt

--- raspberry/src/gps.py
+++ raspberry/src/gps.py
@@ -1,21 +1,21 @@
 import serial
-import time
 import sys
 import pynmea2
 import threading
 
 
 class GPS(threading.Thread):
-    def __init__(self):
+    def __init__(self, run_event):
         self.port = '/dev/ttyS0'
         self.speed = 9600
-        self.msg = None
+        self.msg = pynmea2.parse("$GPGGA,,,,,,,,,,,,,,") # initialize empty msg
+        self.run_event = run_event
 
     def run(self):
         try:
             my_serial = serial.Serial(self.port, self.speed, timeout=5)
 
-            while True:
+            while self.run_event.is_set():
                 data = my_serial.readline()
                 if data.startswith("$GPGGA"):
                     self.msg = pynmea2.parse(data)
--- raspberry/src/main.py
+++ raspberry/src/main.py
@@ -1,11 +1,20 @@
 import time
 import gps
+import threading
 
 if __name__ == "__main__":
-    my_gps = gps.GPS()
+    run_event = threading.Event()
+    run_event.set()
+
+    my_gps = gps.GPS(run_event)
     my_gps.start()
 
-    while True:
-        print ("Latitude: " + str(my_gps.msg.latitude))
-        print ("Longitude: " + str(my_gps.msg.longitude))
-        time.sleep(1)
+    try:
+        while True:
+            print ("Latitude: " + str(my_gps.msg.latitude))
+            print ("Longitude: " + str(my_gps.msg.longitude))
+            time.sleep(1)
+    except KeyboardInterrupt:
+        run_event.clear()
+        my_gps.join()
+        print "Closed all threads successfully"
