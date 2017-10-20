7c1c7276138ebb6784b167d63964eb2c1aa1fa31
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2017-10-20 20:22:26 +0000

    Update gps file

--- raspberry/src/gps.py
+++ raspberry/src/gps.py
@@ -1,22 +1,19 @@
 import serial
 import time
 import sys
-from micropyGPS import MicropyGPS
+import pynmea2
 
 port = '/dev/ttyS0'
-my_gps = MicropyGPS()
 
 try:
     my_serial = serial.Serial(port, 9600, timeout=5)
 
     while True:
-        data = my_serial.read(16)
-        my_gps.update(data)
-
-        print my_gps.latitude
-        print my_gps.longitude
-        print my_gps.speed
-        time.sleep(1)
+        data = my_serial.readline()
+	if (data.startswith("$GPGGA")):
+		msg = pynmea2.parse(data)
+        	print 'Latitude: ' + str(msg.latitude)
+        	print 'Longitude: ' + str(msg.longitude)
 
 except Exception as e:
     sys.stderr.write('Error reading serial port %s: %s\n' % (type(e).__name__, e))
