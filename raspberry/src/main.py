05003168af32212890ce163f5c2563531e9afcef
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2017-10-23 11:52:47 -0200

    Log into a file

--- raspberry/src/main.py
+++ raspberry/src/main.py
@@ -2,6 +2,7 @@ import time
 import gps
 import accelerometer
 import threading
+import os
 
 if __name__ == "__main__":
     run_event = threading.Event()
@@ -15,18 +16,33 @@ if __name__ == "__main__":
     my_accelerometer = accelerometer.Accelerometer(run_event)
     my_accelerometer.start()
 
-    # keeps forever in the loop until a keyboard interrupt is detected
-    try:
-        while True:
-            print ("Latitude: " + str(my_gps.latitude))
-            print ("Longitude: " + str(my_gps.longitude))
-            print ("Speed: " + str(my_gps.speed))
-            print ("Accelerometer X: " + str(my_accelerometer.accel_x))
-            print ("Accelerometer Y: " + str(my_accelerometer.accel_y))
-            print ("Accelerometer Z: " + str(my_accelerometer.accel_z))
-            time.sleep(1)
-    except KeyboardInterrupt:
-        run_event.clear()
-        my_gps.join()
-        my_accelerometer.join()
-        print "Closed all threads successfully!"
+    # creates folder if it doesn't exist
+    folder_path = "../data"
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    # create file in this folder
+    with open(folder_path + "/data.csv", 'w') as f:
+        f.write("Accelerometer X,")
+        f.write("Accelerometer Y,")
+        f.write("Accelerometer Z,")
+        f.write("Latitude,")
+        f.write("Longitude,")
+        f.write("Speed,")
+        f.write("Time,\n")
+
+        # keeps forever in the loop until a keyboard interrupt is detected
+        try:
+            while True:
+                f.write(str(my_accelerometer.accel_x) + ",")
+                f.write(str(my_accelerometer.accel_y) + ",")
+                f.write(str(my_accelerometer.accel_z) + ",")
+                f.write(str(my_gps.latitude) + ",")
+                f.write(str(my_gps.longitude) + ",")
+                f.write(str(my_gps.speed) + ",")
+                f.write(str(time.time()) + "\n")
+                time.sleep(0.01)
+        except KeyboardInterrupt:
+            run_event.clear()
+            my_gps.join()
+            my_accelerometer.join()
+            print "Closed all threads successfully!"
