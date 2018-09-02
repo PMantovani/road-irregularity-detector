a5962e4d9ee6af18aaa66ed49e390ede9be5d126
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2018-09-02 12:47:14 -0300

    Put enable_gps outside of infinite loop

--- raspberry/src/gps_output.py
+++ raspberry/src/gps_output.py
@@ -2,7 +2,7 @@ import serial
 from GsmDevice import GsmDevice
 
 my_serial = serial.Serial('/dev/ttyS0', 115200, timeout=5)
+gps = GsmDevice(my_serial)
+gps.enable_gps()
 while True:
-    gps = GsmDevice(my_serial)
-    gps.enable_gps()
     print(gps.get_gps_info())
