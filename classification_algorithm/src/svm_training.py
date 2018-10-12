b509322518d0fd65b7bb323a6f1b8b5388df3796
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2018-10-11 22:00:50 -0300

    Make it generic for command line arguments

--- classification_algorithm/src/svm_training.py
+++ classification_algorithm/src/svm_training.py
@@ -13,11 +13,51 @@ training_classes = []
 test_data = []
 test_classes = []
 
-axis_indep = False
-two_classes = True
-
 path = 'C:\\Users\\pmant\\Documents\\Repositories\\road-irregularity-detector\\data\\'
 filename = path + 'processed_data'
+
+# parse number of classes
+if '-t' in sys.argv or '--two' in sys.argv:
+    two_classes = True
+else:
+    two_classes = False
+
+# parse axis independence
+if '-a' in sys.argv or '--axis-independent' in sys.argv:
+    axis_indep = True
+else:
+    axis_indep = False
+
+c_parameter = 1
+# parse value of C
+if '-c' in sys.argv:
+    ind = sys.argv.index('-c')
+    c_parameter = float(sys.argv[ind+1])
+
+# parse value of gamma
+gamma_parameter = 1
+if '-g' in sys.argv:
+    ind = sys.argv.index('-g')
+    gamma_parameter = float(sys.argv[ind+1])
+
+# parse kernel type
+kernel_type = 'linear'
+if '-k' in sys.argv:
+    ind = sys.argv.index('-k')
+    kernel_type = sys.argv[ind+1]
+
+
+if two_classes:
+    print 'Rodando classificador para 2 classes'
+else:
+    print 'Rodando classificador para 3 classes'
+if axis_indep:
+    print 'Rodando com independencia de eixo'
+else:
+    print 'Rodando sem independencia de eixo'
+print 'Tipo de kernel: ' + str(kernel_type)
+print 'Parametros de classificacao: C=' + str(c_parameter) + ' gamma=' + str(gamma_parameter)
+
 if two_classes:
     filename += '_2'
 if axis_indep:
@@ -27,7 +67,7 @@ filename += '.csv'
 with open(filename, 'r') as p_data:
     csv_reader = csv.reader(p_data)
     to_training_data = True
-    first_row = True    
+    first_row = True
 
     for row in csv_reader:
         if not first_row:
@@ -38,39 +78,32 @@ with open(filename, 'r') as p_data:
                 if axis_indep:
                     training_data.append([r[1], r[2], r[3], r[4], r[5], r[6]])
                 else:
-                    training_data.append([r[1], r[2], r[3], r[4], r[5], r[6], r[7], 
-                                        r[8], r[9], r[10], r[11], r[12], r[13], r[14]])
-                # training_data.append([ r[1], r[2], r[3], r[4], r[5], r[6], r[8], r[10], r[12], r[13], r[14] ])
+                    training_data.append([r[1], r[2], r[3], r[4], r[5], r[6], r[7],
+                                          r[8], r[9], r[10], r[11], r[12], r[13], r[14]])
                 to_training_data = False
             else:
                 test_classes.append(int(r[0]))
                 if axis_indep:
                     test_data.append([r[1], r[2], r[3], r[4], r[5], r[6]])
                 else:
-                    test_data.append([r[1], r[2], r[3], r[4], r[5], r[6], r[7], 
-                                    r[8], r[9], r[10], r[11], r[12], r[13], r[14]])
-                # test_data.append([ r[1], r[2], r[3], r[4], r[5], r[6], r[8], r[10], r[12], r[13], r[14] ])
+                    test_data.append([r[1], r[2], r[3], r[4], r[5], r[6], r[7],
+                                      r[8], r[9], r[10], r[11], r[12], r[13], r[14]])
                 to_training_data = True
         first_row = False
 
-    c_parameter = float(sys.argv[1])
-    gamma_parameter = float(sys.argv[2])
-    print('Parametros de classificacao: C='+str(c_parameter)+ ' gamma='+str(gamma_parameter))
-
-    classifier = svm.SVC(gamma=gamma_parameter, C=c_parameter, kernel='poly', degree=2)
-    # classifier = svm.SVC(kernel='linear', C=c_parameter)
+    classifier = svm.SVC(gamma=gamma_parameter, C=c_parameter, kernel=kernel_type, degree=2)
     classifier.fit(training_data, training_classes)
     predicted = classifier.predict(test_data)
 
     np.set_printoptions(precision=2, suppress=True)
 
     conf_matrix = np.transpose(confusion_matrix(test_classes, predicted))
-    print('Confusion Matrix: ')
-    print(str(conf_matrix))
-    print('Precision Score: ' + str(precision_score(test_classes, predicted, average='macro')))
-    print('Recall Score: ' + str(recall_score(test_classes, predicted, average='macro')))
-    print('F1 Score: ' + str(f1_score(test_classes, predicted, average='macro')))
-    print('F1 Score (per class): ' + str(f1_score(test_classes, predicted, average=None)))
+    print 'Confusion Matrix: '
+    print str(conf_matrix)
+    print 'Precision Score: ' + str(precision_score(test_classes, predicted, average='macro'))
+    print 'Recall Score: ' + str(recall_score(test_classes, predicted, average='macro'))
+    print 'F1 Score: ' + str(f1_score(test_classes, predicted, average='macro'))
+    print 'F1 Score (per class): ' + str(f1_score(test_classes, predicted, average=None))
 
     # save model to file
     pickle.dump(classifier, open('../../data/model.sav', 'wb'))
