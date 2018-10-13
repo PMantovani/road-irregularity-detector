b0354e89bd5484ac19b77ac0fc33bb3099da421d
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2018-10-13 14:21:36 -0300

    Insert tests for i2cdevice

--- raspberry/test/i2cdevice_test.py
+++ raspberry/test/i2cdevice_test.py
@@ -4,20 +4,20 @@ import unittest
 import sys
 sys.path.insert(1, '../src')
 # noinspection PyUnresolvedReferences
-import accelerometer
+from I2CDevice import I2CDevice
 
 
-class AccelerometerTest(unittest.TestCase):
+class I2CDeviceTest(unittest.TestCase):
 
     def test_merge_bits(self):
-        res = accelerometer.Accelerometer.merge_bits(0b10101110, 0b001, 3, 3)
+        res = I2CDevice.merge_bits(0b10101110, 0b001, 3, 3)
         self.assertEqual(res, 0b10100010)
 
-        res = accelerometer.Accelerometer.merge_bits(0b00000000, 0b1111, 7, 4)
+        res = I2CDevice.merge_bits(0b00000000, 0b1111, 7, 4)
         self.assertEqual(res, 0b11110000)
 
         # tests overflow in new value
-        res = accelerometer.Accelerometer.merge_bits(0b00000000, 0b111111, 2, 2)
+        res = I2CDevice.merge_bits(0b00000000, 0b111111, 2, 2)
         self.assertEqual(res, 0b00000110)
 
 unittest.main()
--- run_py_tests.sh
+++ run_py_tests.sh
@@ -2,3 +2,6 @@
 
 cd classification_algorithm/test
 python test_continuous_transformer.py
+
+cd ../../raspberry/test
+python i2cdevice_test.py
