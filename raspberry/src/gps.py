e78a15edab2475c25b7841cbe00541d0edb17299
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2018-10-29 18:58:17 -0300

    Enable time parsing from $GPRMC

--- raspberry/src/gps.py
+++ raspberry/src/gps.py
@@ -1,5 +1,6 @@
 from threading import Thread
 import time
+from datetime import datetime
 
 class GPS(Thread):
     def __init__(self, serial, lock):
@@ -9,6 +10,7 @@ class GPS(Thread):
         self.latitude = 0.
         self.longitude = 0.
         self.speed = 0.
+        self.current_time = None
         self.lock = lock
 
     @staticmethod
@@ -37,6 +39,9 @@ class GPS(Thread):
             lat_raw = float(params[3])
             lng_raw = float(params[5])
             speed_raw = float(params[7])
+            hour, minute, second = self.__convert_time(params[1])
+            year, month, day = self.__convert_date(params[9])
+            self.current_time = datetime(year, month, day, hour, minute, second)
 
             # calculates latitude, longitude and speed
             self.latitude = round(self.calculate_decimal(lat_raw, params[4]), 7)
@@ -62,6 +67,20 @@ class GPS(Thread):
 
     def is_valid(self):
         return self.signal_validity
+
+    def get_current_time(self):
+        return self.current_time
+
+    def __convert_time(self, time_str):
+        return int(time_str[:2]), int(time_str[2:4]), int(time_str[4:6])
+
+    def __convert_date(self, date_str):
+        year = int(date_str[4:6])
+        if year > 80:
+            year += 1900
+        else:
+            year += 2000
+        return year, int(date_str[2:4]), int(date_str[:2])
 
     def run(self):
         while True:
